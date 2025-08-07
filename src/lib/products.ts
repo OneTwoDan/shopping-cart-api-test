48071dd93ef6a596dbcd37eeec67c189eaca8f34
OneTwoDan <danielondonoa@gmail.com>
2025-08-07 13:02:42 -0500

    Add product images, update interface, and clean global styles

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,10 +1,40 @@
 import { Product } from "./types"
 
 export const products: Product[] = [
-  { id: 1, name: "Cozy Knit Sweater", price: 49.99 },
-  { id: 2, name: "Classic Denim Jeans", price: 59.99 },
-  { id: 3, name: "Leather Ankle Boots", price: 89.0 },
-  { id: 4, name: "Cotton T-Shirt", price: 19.99 },
-  { id: 5, name: "Wool Scarf", price: 29.99 },
-  { id: 6, name: "Canvas Tote Bag", price: 14.99 },
+  {
+    id: 1,
+    name: "Cozy Knit Sweater",
+    price: 49.99,
+    image: "/products/sweater.png",
+  },
+  {
+    id: 2,
+    name: "Classic Denim Jeans",
+    price: 59.99,
+    image: "/products/jeans.png",
+  },
+  {
+    id: 3,
+    name: "Leather Ankle Boots",
+    price: 89.0,
+    image: "/products/boots.png",
+  },
+  {
+    id: 4,
+    name: "Cotton T-Shirt",
+    price: 19.99,
+    image: "/products/tshirt.png",
+  },
+  {
+    id: 5,
+    name: "Wool Scarf",
+    price: 29.99,
+    image: "/products/scarf.png",
+  },
+  {
+    id: 6,
+    name: "Canvas Tote Bag",
+    price: 14.99,
+    image: "/products/tote.png",
+  },
 ]
